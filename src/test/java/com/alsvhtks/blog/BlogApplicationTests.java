3dba1b9206db40f7647ae96a45338390076ddca6
alsgudrl132 <alsgudrl132@gmail.com>
2023-12-04 13:27:34 +0900

    간단한 디자인 html,css

---
+++ src/test/java/com/alsvhtks/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsvhtks.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
