48923d09e55d00d9874467b3d40f73e01b89b636
alsgudrl132 <alsgudrl132@gmail.com>
2023-12-04 16:17:53 +0900

    모달창 추가

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutModal = document.getElementById("aboutModal");
+    const openAboutBtn = document.getElementById("openAboutModalBtn");
+    const closeAboutBtn = document.getElementById("closeAboutModalBtn");
+
+    openAboutBtn.addEventListener("click", function() {
+        aboutModal.style.display = "block";
+    });
+
+    closeAboutBtn.addEventListener("click", function() {
+        aboutModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === aboutModal) {
+            aboutModal.style.display = "none";
+        }
+    });
+
+    const projectsModal = document.getElementById("projectsModal");
+    const openProjectsBtn = document.getElementById("openProjectsModalBtn");
+    const closeProjectsBtn = document.getElementById("closeProjectsModalBtn");
+
+    openProjectsBtn.addEventListener("click", function() {
+        projectsModal.style.display = "block";
+    });
+
+    closeProjectsBtn.addEventListener("click", function() {
+        projectsModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === projectsModal) {
+            projectsModal.style.display = "none";
+        }
+    });
+
+    const githubModal = document.getElementById("githubModal");
+    const openGithubBtn = document.getElementById("openGithubModalBtn");
+    const closeGithubBtn = document.getElementById("closeGithubModalBtn");
+
+    openGithubBtn.addEventListener("click", function() {
+        githubModal.style.display = "block";
+    });
+
+    closeGithubBtn.addEventListener("click", function() {
+        githubModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === githubModal) {
+            githubModal.style.display = "none";
+        }
+    });
+
+    const notionModal = document.getElementById("notionModal");
+    const openNotionBtn = document.getElementById("openNotionModalBtn");
+    const closeNotionBtn = document.getElementById("closeNotionModalBtn");
+
+    openNotionBtn.addEventListener("click", function() {
+        notionModal.style.display = "block";
+    });
+
+    closeNotionBtn.addEventListener("click", function() {
+        notionModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === notionModal) {
+            notionModal.style.display = "none";
+        }
+    });
+    const blogModal = document.getElementById("blogModal");
+    const openBlogBtn = document.getElementById("openBlogModalBtn");
+    const closeBlogBtn = document.getElementById("closeBlogModalBtn");
+
+    openBlogBtn.addEventListener("click", function() {
+        blogModal.style.display = "block";
+    });
+
+    closeBlogBtn.addEventListener("click", function() {
+        blogModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === blogModal) {
+            blogModal.style.display = "none";
+        }
+    });
+});
