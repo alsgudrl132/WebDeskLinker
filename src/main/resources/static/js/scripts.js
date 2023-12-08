6f53283d07340e43178417118949ef25a1b2b597
alsgudrl132 <alsgudrl132@gmail.com>
2023-12-08 14:41:04 +0900

    AWS test

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -35,56 +35,70 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    const githubModal = document.getElementById("githubModal");
-    const openGithubBtn = document.getElementById("openGithubModalBtn");
-    const closeGithubBtn = document.getElementById("closeGithubModalBtn");
+//    const githubModal = document.getElementById("githubModal");
+//    const openGithubBtn = document.getElementById("openGithubModalBtn");
+//    const closeGithubBtn = document.getElementById("closeGithubModalBtn");
+//
+//    openGithubBtn.addEventListener("click", function() {
+//        githubModal.style.display = "block";
+//    });
+//
+//    closeGithubBtn.addEventListener("click", function() {
+//        githubModal.style.display = "none";
+//    });
+//
+//    window.addEventListener("click", function(event) {
+//        if (event.target === githubModal) {
+//            githubModal.style.display = "none";
+//        }
+//    });
+//
+//    const notionModal = document.getElementById("notionModal");
+//    const openNotionBtn = document.getElementById("openNotionModalBtn");
+//    const closeNotionBtn = document.getElementById("closeNotionModalBtn");
+//
+//    openNotionBtn.addEventListener("click", function() {
+//        notionModal.style.display = "block";
+//    });
+//
+//    closeNotionBtn.addEventListener("click", function() {
+//        notionModal.style.display = "none";
+//    });
+//
+//    window.addEventListener("click", function(event) {
+//        if (event.target === notionModal) {
+//            notionModal.style.display = "none";
+//        }
+//    });
+//    const blogModal = document.getElementById("blogModal");
+//    const openBlogBtn = document.getElementById("openBlogModalBtn");
+//    const closeBlogBtn = document.getElementById("closeBlogModalBtn");
+//
+//    openBlogBtn.addEventListener("click", function() {
+//        blogModal.style.display = "block";
+//    });
+//
+//    closeBlogBtn.addEventListener("click", function() {
+//        blogModal.style.display = "none";
+//    });
+//
+//    window.addEventListener("click", function(event) {
+//        if (event.target === blogModal) {
+//            blogModal.style.display = "none";
+//        }
+//    });
 
-    openGithubBtn.addEventListener("click", function() {
-        githubModal.style.display = "block";
-    });
+    const githubLink = "https://github.com/alsgudrl132";
+    const notionLink = "https://imminent-friction-a4d.notion.site/b689df9e5a454f50861de9fa9f02a55b?pvs=4";
 
-    closeGithubBtn.addEventListener("click", function() {
-        githubModal.style.display = "none";
-    });
+    function hrefGithubLink() {
+        window.open(githubLink);
+    }
+    function hrefNotionLink() {
+        window.open(notionLink);
+    }
 
-    window.addEventListener("click", function(event) {
-        if (event.target === githubModal) {
-            githubModal.style.display = "none";
-        }
-    });
+    document.querySelector("#openGithubModalBtn").addEventListener("click",() => hrefGithubLink());
+    document.querySelector("#openNotionModalBtn").addEventListener("click",() => hrefNotionLink());
 
-    const notionModal = document.getElementById("notionModal");
-    const openNotionBtn = document.getElementById("openNotionModalBtn");
-    const closeNotionBtn = document.getElementById("closeNotionModalBtn");
-
-    openNotionBtn.addEventListener("click", function() {
-        notionModal.style.display = "block";
-    });
-
-    closeNotionBtn.addEventListener("click", function() {
-        notionModal.style.display = "none";
-    });
-
-    window.addEventListener("click", function(event) {
-        if (event.target === notionModal) {
-            notionModal.style.display = "none";
-        }
-    });
-    const blogModal = document.getElementById("blogModal");
-    const openBlogBtn = document.getElementById("openBlogModalBtn");
-    const closeBlogBtn = document.getElementById("closeBlogModalBtn");
-
-    openBlogBtn.addEventListener("click", function() {
-        blogModal.style.display = "block";
-    });
-
-    closeBlogBtn.addEventListener("click", function() {
-        blogModal.style.display = "none";
-    });
-
-    window.addEventListener("click", function(event) {
-        if (event.target === blogModal) {
-            blogModal.style.display = "none";
-        }
-    });
 });
